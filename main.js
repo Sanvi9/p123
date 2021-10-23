da0170a98d0f2a97afbc12531cf1ba6d5af9267a
Sanvi9 <74234358+Sanvi9@users.noreply.github.com>
2021-10-23 16:00:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+rightWristX = 0;
+leftWristX = 0;
+difference = 0; 
 function setup() {
     canvas = createCanvas(400,400) 
     canvas.position(700,180)
     video = createCapture(VIDEO)
     video.size(500,500)
     poseNet = ml5.poseNet(video,modelLoaded)
-    poseNet.on("pose",gotposes)
+    poseNet.on('pose',gotPoses)
 }
 
 function modelLoaded() {
@@ -12,6 +15,17 @@ function modelLoaded() {
 }
 
 function draw() {
-    fill("skyblue")
-    text("Coding", 200,200)
-}
+    background("lightblue")
+    stroke("darkblue")
+    text("Coding", 200,200)  
+    textSize(difference) 
+    }
+
+    function gotPoses(results) {
+        console.log(results); 
+        rightWristX = results[0].pose.rightWrist.x
+        leftWristX = results[0].pose.leftWrist.x
+        console.log(rightWristX,leftWristX,difference);
+        difference = floor(leftWristX - rightWristX)
+    
+    }
